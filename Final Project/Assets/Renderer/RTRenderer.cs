ec21c8bc4f99b212bf283cbacc1a9a37435886cd
joeccleung <joeccleung@gmail.com>
2019-02-24 18:28:35 -0800

    [Scene] Sphere renderer for scene parsing

---
+++ Final Project/Assets/Renderer/RTRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class RTRenderer : MonoBehaviour
+{
+    protected RTGeometry m_geom = null;
+
+
+    public RTGeometryType GeometryType
+    {
+        get
+        {
+            if (m_geom == null)
+            {
+                return RTGeometryType.Nothing;
+            }
+
+            return m_geom.GetGeomtryType();
+        }
+    }
+    
+    
+    
+}
