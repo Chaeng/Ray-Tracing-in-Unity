b0ff02f2625560ac8d67abd720797bf32d2ecaaf
joeccleung <joeccleung@gmail.com>
2019-02-24 18:12:20 -0800

    [Geometry] Geometry for sphere

---
+++ Final Project/Assets/Geometry/RTSphere.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSphere : RTGeometry
+{
+    [SerializeField] private float m_radius = 1;
+
+
+    public override RTGeometryType GetGeomtryType()
+    {
+        return RTGeometryType.Sphere;
+    }
+
+
+    public float[] GetGeometry()
+    {
+        var center = transform.position;
+        
+        return new float[4]
+        {
+            center.x,
+            center.y,
+            center.z,
+            m_radius
+        };
+    }
+}
