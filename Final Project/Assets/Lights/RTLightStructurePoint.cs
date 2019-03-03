c1fc80d2a2c63ff01f0aa90931f7f887bc5777ea
joeccleung <joeccleung@gmail.com>
2019-03-03 11:47:24 -0800

    [Shading] Point light structure

---
+++ Final Project/Assets/Lights/RTLightStructurePoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This C# struct corresponding to RTPointLight.compute
+/// </summary>
+public struct RTLightStructurePoint_t
+{
+    public Vector3 color;
+    public Vector3 direction;
+    public Vector3 position;
+
+    public static int GetSize()
+    {
+        return sizeof(float) * 3 * 3;
+    }
+}
