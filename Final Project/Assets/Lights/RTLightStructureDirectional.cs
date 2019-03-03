3b403e8778a57cb70005a72a35bf27c79f7a4c20
joeccleung <joeccleung@gmail.com>
2019-03-03 11:49:56 -0800

    [Shading] Reformat Direction Light Structure

--- Final Project/Assets/Lights/RTLightStructureDirectional.cs
+++ Final Project/Assets/Lights/RTLightStructureDirectional.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// This C# struct corresponding to RTDirectionLight.compute
+/// </summary>
 public struct RTLightStructureDirectional_t
 {
     public Vector3 color;
     public Vector3 direction;
-    
+
     public static int GetSize()
     {
         return sizeof(float) * 3 * 2;
