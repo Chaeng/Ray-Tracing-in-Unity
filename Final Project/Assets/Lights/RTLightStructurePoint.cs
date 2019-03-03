e3fe699ff630fbc023f4f29fa45b13cf8c9f59a9
joeccleung <joeccleung@gmail.com>
2019-03-03 12:20:19 -0800

    [Shading]. Remove unnecessary point light direction information from the structure

--- Final Project/Assets/Lights/RTLightStructurePoint.cs
+++ Final Project/Assets/Lights/RTLightStructurePoint.cs
@@ -6,11 +6,10 @@
 public struct RTLightStructurePoint_t
 {
     public Vector3 color;
-    public Vector3 direction;
     public Vector3 position;
 
     public static int GetSize()
     {
-        return sizeof(float) * 3 * 3;
+        return sizeof(float) * 3 * 2;
     }
 }
