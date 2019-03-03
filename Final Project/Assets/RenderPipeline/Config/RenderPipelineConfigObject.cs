4780660a0c14741ab585aa952240d347ef80bfb3
joeccleung <joeccleung@gmail.com>
2019-03-02 20:35:53 -0800

    [Common] Using scriptable object to hold render pipeline configuration

---
+++ Final Project/Assets/RenderPipeline/Config/RenderPipelineConfigObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Render Pipeline Config")]
+public class RenderPipelineConfigObject : ScriptableObject
+{
+    public Texture skybox = new Texture2D(2, 2);
+    
+    public Color ambitent = Color.white;
+
+    public int secondaryRayGeneration = 0;
+
+    public bool enableShadow = false;
+
+    
+}
