66462b8ad6bc0d8f7ba6ac958b3b2f0a3f6d4afa
kaihua <kaihuh@uw.edu>
2019-03-06 19:33:05 -0800

    Seperate render pipeline file into small files

--- Final Project/Assets/RenderPipeline/SRP/RP_ShadowMap.cs
+++ Final Project/Assets/RenderPipeline/SRP/RP_ShadowMap.cs
@@ -17,36 +17,41 @@ public partial class RayTracingRenderPipeline
             m_shadowMap = new RenderTexture(m_shadowMapWidth, m_shadowMapHeight, 0,
                                             RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             m_shadowMap.enableRandomWrite = true;
+            //m_shadowMap.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
+            //m_shadowMap.volumeDepth = 3;
             m_shadowMap.Create();
         }
+
+        int kIndex = m_shadowMapShader.FindKernel("ShadowMap");
 
         m_shadowMapShader.SetFloats("_SpotLightDir", spotLightDirection.ToArray());
         m_shadowMapShader.SetFloats("_SpotLightPos", spotLightPosition.ToArray());
         m_shadowMapShader.SetInt("_NumOfSpheres", numOfSphere);
-        m_shadowMapShader.SetBuffer(0, "_Spheres", spheres);
+        m_shadowMapShader.SetBuffer(kIndex, "_Spheres", spheres);
         m_shadowMapShader.SetInt("_NumOfTriangles", numofTriangle);
-        m_shadowMapShader.SetBuffer(0, "_Triangles", triangles);
-
-        m_shadowMapShader.SetTexture(0, "Result", m_shadowMap);
+        m_shadowMapShader.SetBuffer(kIndex, "_Triangles", triangles);
+      
+        m_shadowMapShader.SetTextureFromGlobal(kIndex, "_ShadowMap1", "_ShadowMap");
         int threadGroupsX = Mathf.CeilToInt(m_shadowMapWidth / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(m_shadowMapHeight / 8.0f);
-        if (threadGroupsX > 0 && threadGroupsY > 0
-        ) // Prevent dispatching 0 threads to GPU (when the editor is starting or there is no screen to render) 
+
+        // Prevent dispatching 0 threads to GPU (when the editor is starting or there is no screen to render) 
+        if (threadGroupsX > 0 && threadGroupsY > 0) 
         {
-            m_shadowMapShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+            m_shadowMapShader.Dispatch(kIndex, threadGroupsX, threadGroupsY, 1);
         }
 
 
-        /**
-        The below lines of code are for Kai to debug
-         */
+        ///**
+        //The below lines of code are for Kai to debug
+        // */
 
-        RenderTexture.active = m_shadowMap;
-        Texture2D tempResult = new Texture2D(m_shadowMap.width, m_shadowMap.height, TextureFormat.ARGB32, false, false);
-        tempResult.ReadPixels(new Rect(0, 0, m_shadowMap.width, m_shadowMap.height), 0, 0);
-        tempResult.Apply();
+        //RenderTexture.active = m_shadowMap;
+        //Texture2D tempResult = new Texture2D(m_shadowMap.width, m_shadowMap.height, TextureFormat.ARGB32, false, false);
+        //tempResult.ReadPixels(new Rect(0, 0, m_shadowMap.width, m_shadowMap.height), 0, 0);
+        //tempResult.Apply();
 
-        // You should be able to get the result of each pixel form here
-        Vector4 result00 = tempResult.GetPixel(0, 0);
+        //// You should be able to get the result of each pixel form here
+        //Vector4 result00 = tempResult.GetPixel(0, 0);
     }
 }
