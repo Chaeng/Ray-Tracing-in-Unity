5f69b08b9decd2b0e5a2a9408ee7282a4b715c2f
kaihua <kaihuh@uw.edu>
2019-03-08 17:45:18 -0800

    [remove useless content]

--- Final Project/Assets/Lights/RTLightStructureSpot.cs
+++ Final Project/Assets/Lights/RTLightStructureSpot.cs
@@ -9,33 +9,9 @@ public struct RTLightStructureSpot_t
     public Vector3 position;
     public Vector3 direction;
     public float coneAngle;
-    public Vector3 U;
-    public Vector3 W;
-    public Vector3 Pref;
-    public float PixelSize;
 
     public static int GetSize()
     {
-        return sizeof(float) * 3 * 6 + sizeof(float) * 2;
-    }
-
-    public void SetU(Vector3 input)
-    {
-        U = input;
-    }
-
-    public void SetW(Vector3 input)
-    {
-        W = input;
-    }
-
-    public void SetPref(Vector3 input)
-    {
-        Pref = input;
-    }
-
-    public void SetPixelSize(float input)
-    {
-        PixelSize = input;
+        return sizeof(float) * 3 * 3 + sizeof(float);
     }
 }
