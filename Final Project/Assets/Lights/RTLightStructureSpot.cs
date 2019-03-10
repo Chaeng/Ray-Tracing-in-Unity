cf21f2ec3654b8e87fd84792d2afa72058b8c66f
kaihua <kaihuh@uw.edu>
2019-03-09 17:21:11 -0800

    [shadow map] Support shadow UI for spot light

--- Final Project/Assets/Lights/RTLightStructureSpot.cs
+++ Final Project/Assets/Lights/RTLightStructureSpot.cs
@@ -10,9 +10,10 @@ public struct RTLightStructureSpot_t
     public Vector3 direction;
     public float coneAngle;
     public float cosConeAngle;
+    public int shadowMap;
 
     public static int GetSize()
     {
-        return sizeof(float) * 3 * 3 + sizeof(float) * 2;
+        return sizeof(float) * 3 * 3 + sizeof(float) * 2 + sizeof(int);
     }
 }
