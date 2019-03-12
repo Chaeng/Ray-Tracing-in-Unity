b403fb8e2f66ad6cf28dd215f8baef29f1e630a0
andrew.chekerylla <andrew.chekerylla@gmail.com>
2019-03-11 23:00:00 -0700

    [Materials] Fix RTMaterialDatabase bug

--- Final Project/Assets/SceneResourceSupport/Materials/RTMaterialDatabase.cs
+++ Final Project/Assets/SceneResourceSupport/Materials/RTMaterialDatabase.cs
@@ -6,17 +6,17 @@ public class RTMaterialDatabase : MonoBehaviour
 {
     private RTMaterial[] m_materials = null;
 
-    private void Awake()
-    {
-        m_materials = GetComponents<RTMaterial>();
-    }
-
     /// <summary>
     /// This returns material type struct for renderer
     /// </summary>
     /// <returns>material type struct</returns>
     public List<RTMaterial> GetMaterials()
     {
+        if (m_materials == null)
+        {
+            m_materials = GetComponents<RTMaterial>();
+        }
+
         return m_materials == null
             ? null
             : new List<RTMaterial>(m_materials);
